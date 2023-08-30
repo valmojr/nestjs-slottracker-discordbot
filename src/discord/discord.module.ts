6457b396ff0dd35a1ece271ffe156ba9f79e790c
valmojr <val_mo_jr@hotmail.com>
2023-08-30 17:18:13 -0300

    added more listeners to trigger user and guild services for discord bot events

--- src/discord/discord.module.ts
+++ src/discord/discord.module.ts
@@ -7,6 +7,7 @@ import { DiscordConfigService } from './discord.config.service';
 import { DatabaseService } from 'src/database/database.service';
 import { GuildModule } from 'src/guild/guild.module';
 import { GuildService } from 'src/guild/guild.service';
+import { UserService } from 'src/user/user.service';
 
 @Module({
   imports: [
@@ -22,6 +23,7 @@ import { GuildService } from 'src/guild/guild.service';
     DiscordConfigService,
     DatabaseService,
     GuildService,
+    UserService,
   ],
 })
 export class DiscordModule {}
--- src/discord/discord.service.ts
+++ src/discord/discord.service.ts
@@ -1,10 +1,14 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Once, Context, ContextOf, On } from 'necord';
 import { GuildService } from 'src/guild/guild.service';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class DiscordService {
-  constructor(private readonly guildService: GuildService) {}
+  constructor(
+    private readonly guildService: GuildService,
+    private readonly userService: UserService,
+  ) {}
   private readonly logger = new Logger(DiscordService.name);
 
   @Once('ready')
@@ -21,7 +25,14 @@ export class DiscordService {
   public async onGuildCreate(@Context() [guild]: ContextOf<'guildCreate'>) {
     this.logger.log(`Guild ${guild.name} just added SlotTracker`);
 
-    //this.guildService.createGuild(guild);
+    this.guildService.createGuild(guild);
+
+    const guildMembers = await this.guildService.fetchUsers(guild);
+
+    guildMembers.forEach(async (member) => {
+      await this.userService.createOrUpdateUser(member);
+      await this.userService.addUserToGuild(member, guild);
+    });
   }
 
   @On('guildDelete')
@@ -29,5 +40,26 @@ export class DiscordService {
     this.logger.log(`Guild ${guild.name} just removed SlotTracker`);
 
     this.guildService.removeGuild(guild);
+  }
+
+  @On('guildMemberAdd')
+  async onGuildMemberAdd(
+    @Context() [guildMember]: ContextOf<'guildMemberAdd'>,
+  ) {
+    this.userService.createOrUpdateGuildMember(guildMember);
+
+    this.userService.addGuildMemberToGuild(guildMember, guildMember.guild);
+  }
+
+  @On('guildMemberRemove')
+  async onGuildMemberDelete(
+    @Context() [guildMember]: ContextOf<'guildMemberRemove'>,
+  ) {
+    this.userService.removeGuildMemberFromGuild(guildMember, guildMember.guild);
+  }
+
+  @On('userAvatarUpdate')
+  async onUserAvatarUpdate(@Context() [user]: ContextOf<'userAvatarUpdate'>) {
+    this.userService.createOrUpdateUser(user);
   }
 }
