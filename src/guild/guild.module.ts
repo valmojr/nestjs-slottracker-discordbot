78cf451a8c3052596bb6d426de8d7c65cfd3ccd5
valmojr <val_mo_jr@hotmail.com>
2023-08-31 21:44:09 -0300

    removed controller dependencies

--- src/guild/guild.module.ts
+++ src/guild/guild.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { GuildController } from './guild.controller';
 import { GuildService } from './guild.service';
 import { DatabaseService } from 'src/database/database.service';
 
 @Module({
-  imports: [],
-  controllers: [GuildController],
   providers: [GuildService, DatabaseService],
 })
 export class GuildModule {}
