3db3d2f813b839d9f871c834ddfa94e9e9f325e9
valmojr <val_mo_jr@hotmail.com>
2023-08-31 21:44:00 -0300

    removed controller dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DiscordModule } from './discord/discord.module';
+import { EventModule } from './event/event.module';
 
 @Module({
-  imports: [DiscordModule],
+  imports: [DiscordModule, EventModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
-import { UserController } from './user.controller';
 import { DatabaseService } from 'src/database/database.service';
 
 @Module({
-  imports: [],
-  controllers: [UserController],
   providers: [UserService, DatabaseService],
 })
 export class UserModule {}
